f83a3421a611a2c8e56aef5f748e31d4647ccf05
n0w0f <pvt.nawaf@gmail.com>
2024-02-13 00:00:09 +0100

    feat: update cif tokenizer, add unit test for cif tokenizer

--- src/xtal2txt/tokenizer.py
+++ src/xtal2txt/tokenizer.py
@@ -145,6 +145,8 @@ class CifTokenizer(Xtal2txtTokenizer):
     def __init__(self, vocab_file=CIF_VOCAB, model_max_length=None, padding_length=None, **kwargs):
         super(CifTokenizer, self).__init__(vocab_file, model_max_length=model_max_length, padding_length=padding_length, **kwargs)
 
+    def convert_tokens_to_string(self, tokens):
+        return ''.join(tokens)
 
 class Crystal_llm_Tokenizer(Xtal2txtTokenizer):
     def __init__(self, vocab_file=CRYSTAL_LLM_VOCAB, model_max_length=None, padding_length=None, **kwargs):
