a870f44acebcadade4dac78bb0214d920e1250dd
[RezaAliakbari] <rezakbari99@gmail.com>
2024-03-07 13:25:44 +0100

    update: update test_wyckoff

--- tests/decoder/test_wyckoff.py
+++ tests/decoder/test_wyckoff.py
@@ -1,21 +1,21 @@
 from xtal2txt.core import TextRep
 import os
 
+
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-N2 = TextRep.from_input(os.path.join(THIS_DIR, "..", "data", "N2.cif"))
-srtio3_p1 = TextRep.from_input(os.path.join(THIS_DIR, "..", "data", "SrTiO3_p1.cif"))
-incus2_p1 = TextRep.from_input(os.path.join(THIS_DIR, "..", "data", "InCuS2.cif"))
-
+N2 = TextRep.from_input(os.path.join(THIS_DIR, "..", "data", "N2_p1.cif"))
+Sr = TextRep.from_input(os.path.join(THIS_DIR, "..", "data", "SrTiO3_p1.cif"))
+In = TextRep.from_input(os.path.join(THIS_DIR, "..", "data", "InCuS2_p1.cif"))
 
 def test_get_wyckoff_positions() -> None:
     expected = "Sr 1a\nTi 1b\nO 3c\n"
-    assert srtio3_p1.get_wyckoff_positions() == expected
+    assert Sr.get_wyckoff_positions() == expected
 
 
 def test_get_wycryst() -> None:
     expected = "Sr1 Ti1 O3\n221\nSr 1a\nTi 1b\nO 3c\n"
-    assert srtio3_p1.get_wycryst() == expected
+    assert Sr.get_wycryst() == expected
 
 
 def test_wyckoff_matcher() -> None:
@@ -26,12 +26,12 @@ def test_wyckoff_matcher() -> None:
             break
     
     for i in range(10):
-        if srtio3_p1.wyckoff_matcher(lattice_params=True):
+        if Sr.wyckoff_matcher(lattice_params=True):
             results.append(True)
             break
     
     for i in range(10):
-        if incus2_p1.wyckoff_matcher(lattice_params=True):
+        if In.wyckoff_matcher(lattice_params=True):
             results.append(True)
             break
     
