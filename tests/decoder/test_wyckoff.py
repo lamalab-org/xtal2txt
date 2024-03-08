186c54ba8c9eed021abb396f3979abe8aea77a38
[RezaAliakbari] <rezakbari99@gmail.com>
2024-03-08 10:34:55 +0100

    fix: issues in unit tests for wyckoff methods

--- tests/decoder/test_wyckoff.py
+++ tests/decoder/test_wyckoff.py
@@ -8,6 +8,13 @@ N2 = TextRep.from_input(os.path.join(THIS_DIR, "..", "data", "N2_p1.cif"))
 Sr = TextRep.from_input(os.path.join(THIS_DIR, "..", "data", "SrTiO3_p1.cif"))
 In = TextRep.from_input(os.path.join(THIS_DIR, "..", "data", "InCuS2_p1.cif"))
 
+N2_str = "N4\n198\nN 4a\n"
+
+Sr_str = "Sr1 Ti1 O3\n221\nSr 1a\nTi 1b\nO 3c\n"
+
+In_str = "In4 Cu4 S8\n122\nIn 4b\nCu 4a\nS 8d\n"
+
+
 def test_get_wyckoff_positions() -> None:
     expected = "Sr 1a\nTi 1b\nO 3c\n"
     assert Sr.get_wyckoff_positions() == expected
@@ -19,20 +26,7 @@ def test_get_wycryst() -> None:
 
 
 def test_wyckoff_matcher() -> None:
-    results = []
-    for i in range(10):
-        if N2.wyckoff_matcher(lattice_params=True):
-            results.append(True)
-            break
     
-    for i in range(10):
-        if Sr.wyckoff_matcher(lattice_params=True):
-            results.append(True)
-            break
-    
-    for i in range(10):
-        if In.wyckoff_matcher(lattice_params=True):
-            results.append(True)
-            break
-    
-    assert [True, True, True] == results
+    assert N2.wyckoff_matcher(N2_str, lattice_params=True) == True
+    assert Sr.wyckoff_matcher(Sr_str, lattice_params=True) == True
+    assert In.wyckoff_matcher(In_str, lattice_params=True) == True
