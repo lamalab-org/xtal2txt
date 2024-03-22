597966d876a4de40f8d4e09c6dae41b0dfe076eb
n0w0f <pvt.nawaf@gmail.com>
2024-03-22 12:13:32 +0100

    test: update unit test

--- tests/test_get_atom_params.py
+++ tests/test_get_atom_params.py
@@ -4,10 +4,13 @@ import os
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 srtio3_p1 = TextRep.from_input(os.path.join(THIS_DIR, "data", "SrTiO3_p1.cif"))
+tiCrSe_p1 = TextRep.from_input(os.path.join(THIS_DIR, "data", "TlCr5Se8_p1.cif"))
 
 
 def test_get_atom_params() -> None: 
     expected_wo_params = "Sr Ti O O O"
-    expected_w_params = "Sr Ti O O O 90 90 90 3.91 3.91 3.91"
-    assert srtio3_p1.get_atom_params() == expected_wo_params
-    assert srtio3_p1.get_atom_params(lattice_params=True) == expected_w_params
+    expected_w_params = "Sr Ti O O O 3.91 3.91 3.91 90 90 90"
+    expected_tiCrSe_p1 = "Tl Tl Cr Cr Cr Cr Cr Cr Cr Cr Cr Cr Se Se Se Se Se Se Se Se Se Se Se Se Se Se Se Se"
+    assert srtio3_p1.get_atoms_params_rep() == expected_wo_params
+    assert tiCrSe_p1.get_atoms_params_rep() == expected_tiCrSe_p1
+    assert srtio3_p1.get_atoms_params_rep(lattice_params=True) == expected_w_params
