060911970d6251dc01fefca6b34d21c8b5657ed1
Kevin Maik Jablonka <mail@kjablonka.com>
2024-06-02 20:50:30 +0200

    fix tests

--- tests/test_get_atom_params.py
+++ tests/test_get_atom_params.py
@@ -11,6 +11,8 @@ def test_get_atom_params() -> None:
     expected_wo_params = "Sr Ti O O O"
     expected_w_params = "Sr Ti O O O 3.9 3.9 3.9 90 90 90"
     expected_tiCrSe_p1 = "Tl Tl Cr Cr Cr Cr Cr Cr Cr Cr Cr Cr Se Se Se Se Se Se Se Se Se Se Se Se Se Se Se Se"
-    assert srtio3_p1.get_atoms_params_rep() == expected_wo_params
-    assert tiCrSe_p1.get_atoms_params_rep() == expected_tiCrSe_p1
-    assert srtio3_p1.get_atoms_params_rep(lattice_params=True) == expected_w_params
+    assert srtio3_p1.get_atom_sequences_plusplus() == expected_wo_params
+    assert tiCrSe_p1.get_atom_sequences_plusplus() == expected_tiCrSe_p1
+    assert (
+        srtio3_p1.get_atom_sequences_plusplus(lattice_params=True) == expected_w_params
+    )
